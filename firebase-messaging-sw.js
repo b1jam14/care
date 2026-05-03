5215c2b03b23f42caa61139426e047b62b06874e
Benjamin Zolver <48470211+b1jam14@users.noreply.github.com>
2026-05-03 22:56:36 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,46 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "care-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/care/",
+  "/care/index.html",
+  "/care/manifest.json",
+  "/care/icon-192.png",
+  "/care/icon-512.png"
+];
+
+// Installation + mise en cache
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("activate", () => {
+// Activation + nettoyage anciens caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+
   self.clients.claim();
+});
+
+// Mode hors-ligne
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
 
 importScripts("https://www.gstatic.com/firebasejs/12.12.1/firebase-app-compat.js");
